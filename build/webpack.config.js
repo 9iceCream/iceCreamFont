a1599a58fc2ce87872501f526d77a35c87f99350
9iceCream <monolog7@163.com>
2019-07-21 11:55:37 +0800

    init-webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,36 +1,10 @@
 const path = require('path');
 
-const webpack = require('webpack');
-
 module.exports = {
-    mode : 'development',
-    entry:{
-        app:'./src/main.js'
-    },
+    mode: 'development',
+    entry: path.resolve(__dirname,'../src/main.js'),
     output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname,'dist'),
-        publicPath: "./"
-    },
-    module:{
-        rules: [
-            {
-                test : /\.css$/,
-                use : ['style-loader','css-loader']
-            },
-            {
-                test : /\.html$/,
-                loader : 'html-loader'
-            },
-            {
-                test : /\.vue$/,
-                loader : 'vue-loader'
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            vue : 'vue/dist/vue.js'
-        }
+        path: path.resolve(__dirname,'../dist'),
+        filename: '[name].bundle.js'
     }
 }
