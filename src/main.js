feda75cdbfba77f53f0009693866b808c2af4af9
陈刚 <chengang@gsoms.com>
2019-07-23 14:28:52 +0800

    get data

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router/router'
 
+Vue.prototype.$axios = axios
 Vue.use(VueRouter)
 Vue.use(ElementUI)
 
--- src/page/user/Login.vue
+++ src/page/user/Login.vue
@@ -1,12 +1,39 @@
 <template>
     <div>
-        <h1>登录</h1>
+        <el-input
+                placeholder="请输入内容"
+                v-model="username"
+                clearable>
+        </el-input>
+        <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
+        <el-button-group>
+            <el-button type="primary" @click="loginHandle">登录</el-button>
+            <el-button type="info" plain @click="">没有账户？注册</el-button>
+        </el-button-group>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        data:function(){
+            return {
+                username:'',
+                password:''
+            }
+        },
+        methods:{
+            loginHandle:function(){
+                this.$axios.get('http://localhost:8080/hello',{}).then(resp => {
+                    console.log(resp)
+                }).catch(err => {
+                    console.log(err)
+                })
+            },
+            register:function (){
+                console.log("注册")
+            }
+        }
     }
 </script>
 
